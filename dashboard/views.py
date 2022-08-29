5a4391b89d00533761e0654b57715a7be2bb9277
Grovetender <98287387+Grovetender@users.noreply.github.com>
2022-08-29 09:27:09 +0200

    Initial commit

---
+++ dashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('this is the index page')
+
+def staff(request):
+    return HttpResponse('this is the staff page')
